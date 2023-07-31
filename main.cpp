907f72cce05ab1f70dd7dad5fd473f885e20126d
armanapresyan <armanapresyan2020@gmail.com>
2023-07-31 10:03:27 +0400

    update

--- main.cpp
+++ main.cpp
@@ -7,26 +7,26 @@ class Bitset{
      int* v;
     public:
      Bitset(int size) : size(size){
-        v = new int[(size + 31 ) / 32];
+        v = new int[(size + (sizeof(int) * 8 - 1) ) / (sizeof(int) * 8)];
         for (int i = 0; i < (size + 31) / 32; i++) {
             v[i] = 0;
         }
      }
      void set(int position){
-       int index = position / 32;
-       int pos = position % 32;
+       int index = position / (sizeof(int) * 8);
+       int pos = position % (sizeof(int) * 8);
        v[index] |= (1 << pos);
      }
 
      void reset(int position){
-        int index = position / 32;
-       int pos = position % 32;
+        int index = position / (sizeof(int) * 8);
+       int pos = position % (sizeof(int) * 8);
        v[index] &= ~(1 << pos);
      }
 
      void print() {
-          for (int i = (size + 31) / 32 - 1; i >= 0; i--) {
-            for (int j = 31; j >= 0; j--) {
+          for (int i = (size + (sizeof(int) * 8) - 1) / (sizeof(int) * 8) - 1; i >= 0; i--) {
+            for (int j = (sizeof(int) * 8) - 1; j >= 0; j--) {
                 int bit = (v[i] >> j) & 1;
                 std::cout << bit;
             }
