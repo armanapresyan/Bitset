4308edbd349720136b4c6ca503f330820480dcd6
armanapresyan <armanapresyan2020@gmail.com>
2023-07-31 12:50:10 +0400

    add methods

--- main.cpp
+++ main.cpp
@@ -24,6 +24,44 @@ class Bitset{
        v[index] &= ~(1 << pos);
      }
 
+      void flip() {
+        for (int i = 0; i < (size + (sizeof(int) * 8) - 1) / (sizeof(int) * 8); ++i) {
+            v[i] = ~v[i];
+        }
+     }
+
+      bool check() {
+        for (int i = 0; i < (size + (sizeof(int) * 8) - 1) / (sizeof(int) * 8); ++i) {
+          if (v[i] != -1) {
+           return false;
+         }
+        }
+       return true;
+       }
+
+      bool checkOne(){
+        for (int i = 0; i < (size + (sizeof(int) * 8) - 1) / (sizeof(int) * 8); ++i) {
+          if (v[i] != 0) {
+           return true;
+         }
+        }
+        return false;
+      }
+
+
+   int countOne() {
+    int count = 0;
+    for (int i = 0; i < (size + (sizeof(int) * 8) - 1) / (sizeof(int) * 8); i++) {
+        int current = v[i];
+        while (current != 0) {
+            count += current & 1;
+            current >>= 1;
+        }
+    }
+    return count;
+}
+
+
      void print() {
           for (int i = (size + (sizeof(int) * 8) - 1) / (sizeof(int) * 8) - 1; i >= 0; i--) {
             for (int j = (sizeof(int) * 8) - 1; j >= 0; j--) {
@@ -37,16 +75,3 @@ class Bitset{
         delete[] v;
      }
 };
-
-
-int main()
-{
-    Bitset bitset(32);  
-    bitset.set(4);
-    bitset.set(25);
-    bitset.set(5);
-    bitset.reset(4);
-    bitset.print();
-    return 0;
-
-}
